f318f277e8cfcd6160afaef98594a2f691174073
David Bujosa <davidbujosa@gmail.com>
2022-04-30 10:37:54 +0200

    feat: finish login and register logic

--- app/src/main/java/com/bujosa/antares/SplashScreenActivity.java
+++ app/src/main/java/com/bujosa/antares/SplashScreenActivity.java
@@ -4,16 +4,22 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 @SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.splashScreenTheme);
-        new Handler().postDelayed(() -> startActivity(new Intent(SplashScreenActivity.this,
-                MainActivity.class)),2000);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> startActivity(new Intent(SplashScreenActivity.this,
+                LoginActivity.class)),2000);
+
     }
 }
