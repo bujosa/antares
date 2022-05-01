b7c29eb42928e0fb81761ad7fbfb1574b8cec510
David Bujosa <davidbujosa@gmail.com>
2022-05-01 08:51:56 +0200

    refactor: eliminate share preferences

--- app/src/main/java/com/bujosa/antares/MainActivity.java
+++ app/src/main/java/com/bujosa/antares/MainActivity.java
@@ -1,19 +1,11 @@
 package com.bujosa.antares;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.GridView;
 
 import com.bujosa.antares.utils.MenuAdapter;
 import com.bujosa.antares.utils.Menu;
-import com.bujosa.antares.travel.Travel;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,17 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences sharedPreferences = getSharedPreferences("MyPreferences", MODE_PRIVATE );
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        Gson gson = new Gson();
-        String getTravelsJson = sharedPreferences.getString("travels", null);
-        Type type = new TypeToken<ArrayList<Travel>>() {}.getType();
-        List<Travel> travels = gson.fromJson(getTravelsJson, type);
-        if(travels == null){
-            String json = gson.toJson(Travel.generateTravels());
-            editor.putString("travels",json);
-            editor.apply();
-        }
         gridView = findViewById(R.id.gridView);
         gridView.setAdapter(new MenuAdapter(Menu.generateMenu(), this));
     }
