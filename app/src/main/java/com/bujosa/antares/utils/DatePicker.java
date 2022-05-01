acb619066fed730d5f2409fc174e3d79518713c9
David Bujosa <davidbujosa@gmail.com>
2022-05-01 05:31:31 +0200

    feat(utils): create date picker

---
+++ app/src/main/java/com/bujosa/antares/utils/DatePicker.java
@@ -0,0 +1,41 @@
+package com.bujosa.antares.utils;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePicker extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    final OnDateListener onDateListener;
+
+    public DatePicker(OnDateListener onDateListener) {
+        super();
+        this.onDateListener = onDateListener;
+    }
+
+    public interface OnDateListener {
+        void onSelected(int year, int month, int day);
+    }
+
+    @Override
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(android.widget.DatePicker view, int year, int month, int day) {
+        this.onDateListener.onSelected(year, month, day);
+    }
+
+}
