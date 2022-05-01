27c6696912e66aebd436e840b57b27d4a6614e50
David Bujosa <davidbujosa@gmail.com>
2022-05-01 08:53:05 +0200

    feat: replace share preferences for filter input logic

--- app/src/main/java/com/bujosa/antares/travel/FavoritesListTravelActivity.java
+++ app/src/main/java/com/bujosa/antares/travel/FavoritesListTravelActivity.java
@@ -1,23 +1,21 @@
 package com.bujosa.antares.travel;
 
 import android.annotation.SuppressLint;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bujosa.antares.R;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
+
 
 public class FavoritesListTravelActivity extends AppCompatActivity {
 
@@ -30,17 +28,31 @@ public class FavoritesListTravelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.available_travels);
+
         imageButton=findViewById(R.id.imageButton);
         textView=findViewById(R.id.textView2);
+
         textView.setText("Lugares Favoritos");
+
         imageButton.setVisibility(View.INVISIBLE);
         recyclerView=findViewById(R.id.listRecyclerView);
-        SharedPreferences sharedPreferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
-        Gson gson = new Gson();
-        String json = sharedPreferences.getString("mytravels", null);
-        Type type = new TypeToken<ArrayList<Travel>>() {}.getType();
-        List<Travel> travels = gson.fromJson(json, type);
-        recyclerView.setAdapter(new TravelAdapter(travels, this, false));
+
+        TravelService travelService = new TravelService(FirebaseFirestore.getInstance());
+
+        FilterInputs filterInputs = new FilterInputs();
+
+        filterInputs.setFavorite(true);
+
+        travelService.loadTravels(filterInputs).addOnSuccessListener(documentSnapshots -> {
+            List<Travel> types = documentSnapshots.toObjects(Travel.class);
+            recyclerView.setAdapter(new TravelAdapter(types, this, false));
+        }).addOnFailureListener(e -> showMessage("Error loading trips. Please verify. " + e.getMessage()));
+
+
         recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 }
