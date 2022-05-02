fc7e339f39c06a42399e08b81a099fc793b8dc19
David Bujosa <davidbujosa@gmail.com>
2022-05-02 07:34:54 +0200

    feat: add map packages and create map location logic

--- app/src/main/java/com/bujosa/antares/travel/TravelActivity.java
+++ app/src/main/java/com/bujosa/antares/travel/TravelActivity.java
@@ -1,104 +1,113 @@
 package com.bujosa.antares.travel;
 
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 
+import com.bujosa.antares.MainActivity;
 import com.bujosa.antares.R;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
-public class TravelActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    ImageButton imageButton;
-    private AlertDialog dialog;
-    private int minPrice = 0, maxPrice = 9999;
-    private TravelService travelService;
+public class TravelActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == Activity.RESULT_OK) {
-                    loadTravels(null);
-                }
-            });
+    TextView title, secondTitle, description, price, startDate, endDate;
+    ImageView imageView;
+    Button buyButton, mapButton, imageButton;
 
+    private static final int DEFAULT_ZOOM = 17;
+
+    private Travel travel;
+
+    final private DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.available_travels);
+        setContentView(R.layout.activity_travel);
 
-        recyclerView = findViewById(R.id.listRecyclerView);
-        imageButton = findViewById(R.id.imageButton);
+        travel = (Travel) getIntent().getSerializableExtra("Travel");
 
-        travelService = new TravelService(FirebaseFirestore.getInstance());
+        title = findViewById(R.id.detailTitleTextView);
+        secondTitle = findViewById(R.id.itemTextView);
+        description = findViewById(R.id.detailDescriptionTextView);
+        price = findViewById(R.id.detailPriceTextView);
+        imageView = findViewById(R.id.detailImageView);
+        startDate = findViewById(R.id.detailStartDateTextView);
+        endDate = findViewById(R.id.detailEndDateTextView);
+        buyButton = findViewById(R.id.buyButton);
+        imageButton = findViewById(R.id.itemImageButton);
+        mapButton = findViewById(R.id.itemMapButton);
 
-        imageButton.setOnClickListener(view -> showDialog());
 
-        loadTravels(null);
-    }
 
-    public void showDialog(){
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        final View filterView = getLayoutInflater().inflate(R.layout.popup, null);
-        EditText minPriceFilter = filterView.findViewById(R.id.minPriceEditText);
-        EditText maxPriceFilter = filterView.findViewById(R.id.maxPriceEditText);
-        Button filterButton = filterView.findViewById(R.id.applyFilterButton);
-        Button cancelButton = filterView.findViewById(R.id.cancelFilterButton);
+        String priceResult = "" + travel.getPrice();
+        Picasso.get()
+                .load(travel.getImage())
+                .placeholder(android.R.drawable.ic_dialog_map)
+                .error(android.R.drawable.ic_dialog_alert)
+                .into(imageView);
 
-        cancelButton.setOnClickListener(view -> dialog.cancel());
+        title.setText(travel.getTitle());
+        secondTitle.setText(travel.getTitle());
+        description.setText(travel.getDescription());
+        price.setText(priceResult);
+        startDate.setText(formatter.format(travel.getStartDate()));
+        endDate.setText(formatter.format(travel.getEndDate()));
 
-        filterButton.setOnClickListener(view -> {
-            dialog.hide();
-            minPrice = Integer.parseInt(String.valueOf(minPriceFilter.getText()));
-            maxPrice = Integer.parseInt(String.valueOf(maxPriceFilter.getText()));
-            FilterInputs filterInputs = new FilterInputs();
-            filterInputs.setMaxPrice(maxPrice);
-            filterInputs.setMinPrice(minPrice);
-            loadTravels(filterInputs);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_view);
+
+        assert mapFragment != null;
+
+        mapFragment.getMapAsync(this);
+
+        buyButton.setOnClickListener(view -> {
+            Toast toast = Toast.makeText(this,"Haz comprado este viaje", Toast.LENGTH_LONG);
+            toast.show();
+            SystemClock.sleep(2000);
+            startActivity(new Intent(this,
+                    MainActivity.class));
         });
 
-        dialogBuilder.setView(filterView);
-        dialog = dialogBuilder.create();
-        dialog.show();
+        imageButton.setOnClickListener(view -> {
+            findViewById(R.id.cardItemMapView).setVisibility(View.GONE);
+            findViewById(R.id.cardItemView).setVisibility(View.VISIBLE);
+        });
+
+        mapButton.setOnClickListener(view -> {
+            findViewById(R.id.cardItemMapView).setVisibility(View.VISIBLE);
+            findViewById(R.id.cardItemView).setVisibility(View.GONE);
+        });
+
     }
 
-    private void loadTravels(@Nullable FilterInputs filterInputs) {
-        travelService.loadTravels(filterInputs).addOnSuccessListener(documentSnapshots -> {
-            List<Travel> types = documentSnapshots.toObjects(Travel.class);
-            changesRecycleView(new ArrayList<>(types));
-        }).addOnFailureListener(e -> showMessage("Error cargando los viajes " + e.getMessage()));
-    }
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng location = new LatLng(travel.getLatitude(), travel.getLongitude());
 
-    public void addTravel(View view) {
-        Intent intent = new Intent(this, AddTravelActivity.class);
-        activityResultLauncher.launch(intent);
-    }
+        googleMap.addMarker(new MarkerOptions()
+                .position(location)
+                .title(travel.getTitle()));
 
-    public void changesRecycleView(ArrayList<Travel> travels){
-        recyclerView = findViewById(R.id.listRecyclerView);
-        recyclerView.setAdapter(new TravelAdapter(travels, this, true));
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
-    }
-
-    private void showMessage(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, DEFAULT_ZOOM));
     }
 }
